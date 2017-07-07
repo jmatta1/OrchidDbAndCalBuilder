b52c843d35c87da8f06ac0d50780cbf682d6d1c2
James Till Matta <jamesmatta@gmail.com>
2017-07-06 21:04:27 -0400

    Started making datebase operations lib
    Signed-off-by: James Till Matta <jamesmatta@gmail.com>

--- odbblib/__init__.py
+++ odbblib/__init__.py
@@ -1,2 +1,4 @@
 """Simple library for functions and data needed by the orchid_db_builder"""
 import odbblib.schedule as schedule
+import odbblib.readrawdata as readrawdata
+import odbblib.databaseops as databaseops
---
+++ odbblib/databaseops.py
@@ -0,0 +1,81 @@
+"""Functions to create, update, and add to the various databases"""
+import sqlite3 as sql
+import os
+
+BATCH_TABLE_CMD = """CREATE TABLE batch_table (
+    batch_name text PRIMARY KEY,
+    array_x real NOT NULL,
+    array_y real NOT NULL,
+    raw_root_location text NOT NULL,
+    run_data_location text NOT NULL,
+    det_data_location text NOT NULL,
+    tree_gen int NOT NULL,
+    tree_root_location text NOT NULL,
+    hist_integration_time real NOT NULL,
+    start_us_epoch int NOT NULL,
+    start_time text NOT NULL,
+    stop_us_epoch int NOT NULL,
+    stop_time text NOT NULL,
+    run_count int NOT NULL,
+    det_count int NOT NULL,
+    first_buffer_skip int NOT NULL,
+    has_been_calibrated int DEFAULT 0,
+    has_been_decomposed int DEFAULT 0,
+    proc_root_location text DEFAULT '',
+    run_db_location text DEFAULT '',
+    det_db_location text DEFAULT ''
+);
+"""
+
+DICT_NAMES = ["BatchName",          "ArrayX",           "ArrayY",
+              "RootFileLocation",   "RunDataLocation",  "DetDataLocation",
+              "TreeGenerated",      "TreeFileLocation", "IntTime",
+              "StartEpochMicroSec", "StartDateTime",    "StopEpochMicroSec",
+              "StopDateTime",       "RunCount",         "DetCount",
+              "FirstBufferSkipped", "IsCalibrated",     "IsDecomposed",
+              "ProcRootLoc",        "RunDbLoc",         "DetDbLoc"]
+
+
+BATCH_INSERT = "INSERT INTO batch_table VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
+
+
+def add_batch_data(batch_data, db_loc):
+    """Adds a row to the global batch database using the batch data
+    dictionary that was read
+
+    PARAMETERS
+    ----------
+    batch_data : dict
+        dictionary of information to be dumped into the batch database
+
+    RETURN
+    ------
+    success : bool
+        True if successful
+        False if there was an unrecoverable error
+    """
+    # if the database did not already exist it will be created in the connect
+    dbcon = sql.connect(db_loc)
+    cursor = dbcon.cursor()
+    # check if the table exists (in case the db is newly created)
+    try:
+        cursor.execute(BATCH_TABLE_CMD)
+    except sql.OperationalError as err:
+        # if there was an error creating the table then it already exists
+        pass
+    # add the keys to the dictionary containing the extra column values
+    batch_data["IsCalibrated"] = False
+    batch_data["IsDecomposed"] = False
+    batch_data["ProcRootLoc"] = ""
+    batch_data["RunDbLoc"] = ""
+    batch_data["DetDbLoc"] = ""
+    out_list = []
+    for key in DICT_NAMES:
+        if "DateTime" in key:
+            out_list.append(batch_data[key].__str__())
+        elif type(batch_data[key]) is bool:
+            out_list.append(1 if batch_data[key] else 0)
+        else:
+            out_list.append(batch_data[key])
+    cursor.execute(BATCH_INSERT, out_list)
+
