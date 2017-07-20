404eb73a83123e7cb25b38c7d9e21161a1d3d529
James Till Matta <jamesmatta@gmail.com>
2017-07-20 14:18:52 -0400

    Generation of all calibration input files
    Signed-off-by: James Till Matta <jamesmatta@gmail.com>

--- odacblib/rootops.py
+++ odacblib/rootops.py
@@ -2,14 +2,78 @@
 manipulate root files and data"""
 
 import ROOT as rt
+import odacblib.input_sanitizer as ins
 
-def prep_calibration_file(sum_lists, root_input, root_output, run_info):
+def find_sodium_peak_runs(lo_bnd, hi_bnd, root_input):
+    """This function prepares a calibration root file for a single calibration
+    block, when that block happens to fall into the "early reactor off" class
+    It will query the user to deterimine where the 24Na peak dissappears
+    and produce two run data tuples, one with and one without the 24Na peak
+
+    Parameters
+    ----------
+    lo_bnd : int
+        The lowest run in the stretch to be checked
+    hi_bnd : int
+        The highest run in the stretch to be checked
+    root_input : str
+        The path of the root input file
+
+    Returns
+    -------
+    runs_list : list of tuples
+        Each tuple contains a start run and stop run and either a 1 or a 0
+        0 indicates there is no 24Na peak in these runs, 1 indicates there is
+    """
+    query = "Does this spectrum contain a reasonable 24Na peak"
+    # First create a canvas so that we can control drawing
+    canv = rt.TCanvas("c1", "Find 24Na peaks")
+    print "Finding Runs containing a reasonable 24Na peak"
+    infile = rt.TFile(root_input)
+    # first check if there is a 24Na peak at the start of everything
+    fmt = "Det_8_Run_{0:d}_px"
+    out_list = []
+    print "Checking spectrum:", lo_bnd
+    rt.gStyle.SetOptStat(0)
+    hist = infile.Get(fmt.format(lo_bnd))
+    hist.Draw()
+    canv.Update()
+    last_ans = ins.get_yes_no(query, default_value=True)
+    start_run = lo_bnd
+    stop_run = lo_bnd+2
+    x_range = (canv.GetFrame().GetX1(), canv.GetFrame().GetX2())
+    for i in range(lo_bnd+3, hi_bnd+1, 3):
+        print "Checking spectrum:", i
+        hist = infile.Get(fmt.format(i))
+        hist.Draw()
+        canv.Update()
+        hist.GetXaxis().SetRangeUser(x_range[0], x_range[1])
+        hist.Draw()
+        canv.Update()
+        ans = ins.get_yes_no(query, default_value=True)
+        x_range = (canv.GetFrame().GetX1(), canv.GetFrame().GetX2())
+        if last_ans == ans:
+            stop_run = i+2
+        else:
+            if stop_run > hi_bnd:
+                stop_run = hi_bnd
+            out_list.append((start_run, stop_run, 1 if last_ans else 0))
+            start_run = i
+            stop_run = i+2
+            last_ans = ans
+    if stop_run > hi_bnd:
+        stop_run = hi_bnd
+    out_list.append((start_run, stop_run, 1 if last_ans else 0))
+    return out_list
+
+
+def prep_calibration_file(runs, root_input, root_output, det_data, num_runs):
     """This function  generates / copies sums for the calibration file for the
     full calibration program to use
 
     Parameters
     ----------
-    sum_lists : list of tuples
+    runs : list of tuples
         list of tuples where each tuple has the start run, the stop run,
         the gamma-ray list for calibration, and the "kind" of calibration
         0 - reactor on
@@ -19,6 +83,177 @@ def prep_calibration_file(sum_lists, root_input, root_output, run_info):
         The path of the root input file
     root_output : str
         The path of the root output file
+    det_data : list of dict
+        list of dictionary of the detector data
+    num_runs : int
+        The number of runs in this batch
     """
     # first check if we can merely use pregenerated sums or if we need to
     # generate new sums
+    run_count = 1 + runs[0][1] - runs[0][0]
+    if len(runs) == 1 and run_count == num_runs:
+        do_normal_prep(runs, root_input, root_output, det_data)
+    else:
+        do_split_prep(runs, root_input, root_output, det_data)
+
+
+def do_split_prep(runs, root_input, root_output, det_data):
+    """This function prepares a calibration root file for a single calibration
+    block, i.e. all the data is coming from reactor on, or reactor off, no
+    exceptions
+
+    Parameters
+    ----------
+    runs : list of tuples
+        list of tuples where each tuple has the start run, the stop run,
+        the gamma-ray list for calibration, and the "kind" of calibration
+        0 - reactor on
+        1 - reactor off, early (so 24Na peak is visible)
+        2 - reactor off, late (no 24Na peak)
+    root_input : str
+        The path of the root input file
+    root_output : str
+        The path of the root output file
+    det_data : list of dict
+        list of dictionary of the detector data
+    """
+    print "Preparing Root Calibration File"
+    for i, run in enumerate(runs):
+        do_single_prep(run, root_input, root_output, det_data, i)
+    outfile = rt.TFile(root_output, "UPDATE")
+    outfile.cd()
+    num_cals = rt.TParameter('int')("NumCals", 1)
+    num_cals.Write()
+    outfile.Flush()
+
+
+def do_single_prep(run, root_input, root_output, det_data, ind):
+    """This function prepares a calibration root file for a single calibration
+    block, i.e. all the data is coming from reactor on, or reactor off, no
+    exceptions
+
+    Parameters
+    ----------
+    run : tuple
+        tuple where each tuple has the start run, the stop run,
+        the gamma-ray list for calibration, and the "kind" of calibration
+        0 - reactor on
+        1 - reactor off, early (so 24Na peak is visible)
+        2 - reactor off, late (no 24Na peak)
+    root_input : str
+        The path of the root input file
+    root_output : str
+        The path of the root output file
+    det_data : list of dict
+        list of dictionary of the detector data
+    ind : int
+        which calibration we are preparing
+    """
+    print "Preparing calibration #", ind
+    infile = rt.TFile(root_input)
+    arg = "UPDATE" if ind != 0 else "RECREATE"
+    outfile = rt.TFile(root_output, arg)
+    # write a few extra tidbits to the file
+    cal_start = rt.TParameter('int')("Cal_{0:d}_Start".format(ind), run[0])
+    cal_start.Write()
+    cal_stop = rt.TParameter('int')("Cal_{0:d}_Stop".format(ind), run[1])
+    cal_stop.Write()
+    num_gammas = rt.TParameter('int')("Cal_{0:d}_NumGammas".format(ind),
+                                      len(run[2]))
+    num_gammas.Write()
+    for i, gamma in enumerate(run[2]):
+        temp = rt.TParameter('double')("Cal_{0:d}_Gamma_{1:d}".format(ind, i),
+                                       gamma)
+        temp.Write()
+    outfile.Flush()
+    # Now prepare to write the spectra to the file
+    in_fmts = ["Det_{0:d}_Run_{{0:d}}_px", "Det_{0:d}_Run_{{0:d}}_px_thresh",
+               "Det_{0:d}_Run_{{0:d}}_py", "Det_{0:d}_Run_{{0:d}}_px_thresh",
+               "Det_{0:d}_Run_{{0:d}}_2D"]
+    out_fmts = ["Det_{0:d}_Sum_px_Cal_{1:d}",
+                "Det_{0:d}_Sum_px_thresh_Cal_{1:d}",
+                "Det_{0:d}_Sum_py_Cal_{1:d}",
+                "Det_{0:d}_Sum_px_thresh_Cal_{1:d}",
+                "Det_{0:d}_Sum_2D_Cal_{1:d}"]
+    # now copy the sum spectra over to the root file
+    for dat in det_data:
+        print "Performing spectrum sums for Detector: ", dat["DetNum"]
+        # first copy the histograms over
+        in_temps = [x.format(dat["DetNum"]) for x in in_fmts]
+        out_names = [x.format(dat["DetNum"], ind) for x in out_fmts]
+        in_names = [x.format(run[0]) for x in in_temps]
+        out_hists = []
+        for iname, oname in zip(in_names, out_names):
+            inhist = infile.Get(iname)
+            outfile.cd()
+            outhist = inhist.Clone()
+            outhist.SetName(oname)
+            out_hists.append(outhist)
+        for i in range(run[0]+1, run[1]+1):
+            in_names = [x.format(i) for x in in_temps]
+            for iname, outhist in zip(in_names, out_hists):
+                inhist = infile.Get(iname)
+                outhist.Add(inhist, 1.0)
+        for outhist in out_hists:
+            outfile.cd()
+            outhist.Write()
+    outfile.Flush()
+    print "Done preparing calibration #", ind
+
+
+def do_normal_prep(runs, root_input, root_output, det_data):
+    """This function prepares a calibration root file for a single calibration
+    block, i.e. all the data is coming from reactor on, or reactor off, no
+    exceptions
+
+    Parameters
+    ----------
+    runs : list of tuples
+        list of tuples where each tuple has the start run, the stop run,
+        the gamma-ray list for calibration, and the "kind" of calibration
+        0 - reactor on
+        1 - reactor off, early (so 24Na peak is visible)
+        2 - reactor off, late (no 24Na peak)
+    root_input : str
+        The path of the root input file
+    root_output : str
+        The path of the root output file
+    det_data : list of dict
+        list of dictionary of the detector data
+    """
+    # otherwise we can procded as normal
+    # open the root files
+    print "Preparing Root Calibration File"
+    infile = rt.TFile(root_input)
+    outfile = rt.TFile(root_output, "RECREATE")
+    # write a few extra tidbits to the file
+    num_cals = rt.TParameter('int')("NumCals", 1)
+    num_cals.Write()
+    cal_start = rt.TParameter('int')("Cal_0_Start", runs[0][0])
+    cal_start.Write()
+    cal_stop = rt.TParameter('int')("Cal_0_Stop", runs[0][1])
+    cal_stop.Write()
+    num_gammas = rt.TParameter('int')("Cal_0_NumGammas", len(runs[0][2]))
+    num_gammas.Write()
+    for i, gamma in enumerate(runs[0][2]):
+        temp = rt.TParameter('double')("Cal_0_Gamma_{0:d}".format(i), gamma)
+        temp.Write()
+    outfile.Flush()
+    # Now prepare to write the spectra to the file
+    in_fmts = ["Det_{0:d}_Sum_px", "Det_{0:d}_Sum_px_thresh",
+               "Det_{0:d}_Sum_py", "Det_{0:d}_Sum_px_thresh",
+               "Det_{0:d}_Sum_2D"]
+    out_fmts = ["Det_{0:d}_Sum_px_Cal_0", "Det_{0:d}_Sum_px_thresh_Cal_0",
+                "Det_{0:d}_Sum_py_Cal_0", "Det_{0:d}_Sum_px_thresh_Cal_0",
+                "Det_{0:d}_Sum_2D_Cal_0"]
+    # now copy the sum spectra over to the root file
+    for dat in det_data:
+        # first copy the histograms over
+        in_names = [x.format(dat["DetNum"]) for x in in_fmts]
+        out_names = [x.format(dat["DetNum"]) for x in out_fmts]
+        for iname, oname in zip(in_names, out_names):
+            hist = infile.Get(iname)
+            hist.SetName(oname)
+            outfile.cd()
+            hist.Write()
+    outfile.Flush()
